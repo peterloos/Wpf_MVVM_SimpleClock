1c9e0925fd1cb4448cdb2897673e822724955d7f
Peter Loos <peter.loos@gmx.de>
2015-10-09 20:50:11 +0200

    Initial Release

---
+++ Wpf_MVVM_01_SimpleClock/ClockModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Wpf_MVVM_01_SimpleClock
+{
+    public delegate void DateChangedEventHandler(DateTime date);
+    public delegate void TimeChangedEventHandler(TimeSpan time);
+
+    class ClockModel
+    {
+        public event DateChangedEventHandler DateChanged;
+        public event TimeChangedEventHandler TimeChanged;
+
+        private DispatcherTimer dispatcherTimer;
+        private DateTime lastTime;
+
+        public ClockModel()
+        {
+            this.DateChanged = null;
+            this.TimeChanged = null;
+
+            this.lastTime = DateTime.Now;
+
+            this.dispatcherTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            this.dispatcherTimer.Tick += this.TimerTick;
+        }
+
+        public DateTime LastDate
+        {
+            get
+            {
+                return this.lastTime.Date;
+            }
+        }
+
+        public TimeSpan LastTime
+        {
+            get
+            {
+                return this.lastTime.TimeOfDay;
+            }
+        }
+
+        public void Start()
+        {
+            this.dispatcherTimer.Start();
+        }
+
+        public void Stop()
+        {
+            this.dispatcherTimer.Stop();
+        }
+
+        private void TimerTick(Object sender, EventArgs e)
+        {
+            DateTime last = this.lastTime;
+            this.lastTime = DateTime.Now;
+
+            if (this.lastTime.Date != last.Date)
+                if (this.DateChanged != null)
+                    this.DateChanged.Invoke(this.lastTime.Date);
+
+            if (this.lastTime.TimeOfDay != last.TimeOfDay)
+                if (this.TimeChanged != null)
+                    this.TimeChanged.Invoke(this.lastTime.TimeOfDay);
+        }
+    }
+}
---
+++ Wpf_MVVM_01_SimpleClock/ClockViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_MVVM_01_SimpleClock
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ClockModel model;
+
+        private String currentTime;
+        private String currentDate;
+
+        public ClockViewModel()
+        {
+            this.model = new ClockModel();
+
+            this.currentDate = String.Format("{0:00}.{1:00}.{2:0000}",
+                this.model.LastDate.Day,
+                this.model.LastDate.Month,
+                this.model.LastDate.Year);
+
+            this.currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                this.model.LastTime.Hours,
+                this.model.LastTime.Minutes,
+                this.model.LastTime.Seconds);
+
+            this.model.DateChanged += this.Model_DateChanged;
+            this.model.TimeChanged += this.Model_TimeChanged;
+
+            this.model.Start();
+        }
+
+        public String CurrentTime
+        {
+            set
+            {
+                this.currentTime = value;
+
+                if (this.PropertyChanged != null)
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("CurrentTime"));
+            }
+
+            get
+            {
+                return this.currentTime;
+            }
+        }
+
+        public String CurrentDate
+        {
+            set
+            {
+                this.currentDate = value;
+
+                if (this.PropertyChanged != null)
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("CurrentDate"));
+            }
+
+            get
+            {
+                return this.currentDate;
+            }
+        }
+
+        private void Model_TimeChanged(TimeSpan time)
+        {
+            this.CurrentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                time.Hours, time.Minutes, time.Seconds);
+        }
+
+        private void Model_DateChanged(DateTime date)
+        {
+            this.CurrentDate = String.Format("{0:00}.{1:00}.{2:0000}",
+                date.Day, date.Month, date.Year);
+        }
+    }
+}
